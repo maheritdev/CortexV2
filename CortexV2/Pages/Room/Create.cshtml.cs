471b5056238fa793590b616824a094d0c2d8b8fa
Maher Abu Amereh <84269245+maheritdev@users.noreply.github.com>
2026-01-11 01:13:43 +0300

    Enhance Ui Inventory and Rooms.

--- CortexV2/Pages/Room/Create.cshtml.cs
+++ CortexV2/Pages/Room/Create.cshtml.cs
@@ -20,7 +20,7 @@ namespace Cortex.Pages.Room
 
         public IActionResult OnGet()
         {
-        ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
+        ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentName");
             return Page();
         }
 
--- CortexV2/Pages/Room/Edit.cshtml.cs
+++ CortexV2/Pages/Room/Edit.cshtml.cs
@@ -35,7 +35,7 @@ namespace Cortex.Pages.Room
                 return NotFound();
             }
             Room = room;
-           ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
+           ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentName");
             return Page();
         }
 
